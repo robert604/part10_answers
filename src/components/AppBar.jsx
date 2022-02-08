bf62ea1c02bb13b82ac74f5956501cc29ec586e4
robert604 <robtemper@gmail.com>
2022-02-08 11:18:23 -0500

    submission for 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View,StyleSheet } from 'react-native';
+import { View,StyleSheet,ScrollView } from 'react-native';
 import AppBarTab from './AppBarTab';
 import gs from '../globalStyles';
 import theme from '../theme';
@@ -12,9 +12,12 @@ const styles = StyleSheet.create({
 
 const AppBar = () => {
   return (
-    <View style={[styles.appBar,gs.flexContainerRow]}>
-      <AppBarTab title='Repositories' path='/'/>
-      <AppBarTab title='Sign In' path ='/signin'/>
+    <View style={[styles.appBar]}>
+      <ScrollView horizontal>
+        <AppBarTab title='Repositories' path='/'/>
+        <AppBarTab title='Sign In' path ='/signin'/>
+               
+      </ScrollView>
     </View>
   );
 
